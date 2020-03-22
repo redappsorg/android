76f5dd21e36bc294be96eebdd0b9fb9ad337fa4d
redappsorg <62048698+redappsorg@users.noreply.github.com>
2020-03-22 22:09:04 +0430

    Add files via upload

---
+++ 09_LayoutsAndWidgets/CriminalIntent/app/src/main/java/org/redapps/criminalintent/CrimeActivity.java
@@ -0,0 +1,11 @@
+package org.redapps.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeFragment();
+    }
+}
