88e99a146e249fe2a26171274374d81282edcccc
redappsorg <62048698+redappsorg@users.noreply.github.com>
2020-03-22 22:29:04 +0430

    Add files via upload

---
+++ 16_CameraIntent/CriminalIntent/app/src/main/java/org/redapps/criminalintent/CrimeListActivity.java
@@ -0,0 +1,11 @@
+package org.redapps.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+public class CrimeListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
